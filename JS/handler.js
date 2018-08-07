3f419dede42661cfe532f28a97d3ce183dd11c26
Vashali <vaishali2016@outlook.com>
2018-08-07 20:08:03 +0100

    Creating index page

--- JS/handler.js
+++ JS/handler.js
@@ -2,6 +2,15 @@ const express=require('express')
 const app = express()
 const path = require('path')
 
+const bodyParser=require('body-parser')
+
+app.use( bodyParser.json() )       
+app.use(bodyParser.urlencoded({    
+  extended: true
+}))
+
+let userId=undefined
+
 app.get('/register',function(req, res) {
 	res.sendfile(path.resolve(__dirname+'/../HTML/NewUser.html'))
 })
@@ -11,8 +20,22 @@ app.get('/login', function(req,res){
 })
 
 app.post('/welcome', function(req,res){
-	res.sendfile(path.resolve(__dirname+'/../HTML/Welcome.html'))
+	userId=req.body.userId
+
+	if(userId!=undefined){
+		res.sendfile(path.resolve(__dirname+'/../HTML/Welcome.html'))
+	}	
 	
+
+})
+
+app.get('/logout', function(req,res){
+	userId=undefined
+	res.sendfile(path.resolve(__dirname+'/../HTML/Index.html'))
+})
+
+app.get('/', function(req,res){
+	res.sendfile(path.resolve(__dirname+'/../HTML/Index.html'))
 })
 
 app.listen(8080, function() {
